2b17f15e6d029d87f1af68883918b8a143d780b1
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-17 00:05:12 +0200

    Adjusted table header

--- resources/views/theme/table.blade.php
+++ resources/views/theme/table.blade.php
@@ -5,6 +5,7 @@
             <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
+            <th scope="col">Handle</th>
         </tr>
         </thead>
         <tbody>
