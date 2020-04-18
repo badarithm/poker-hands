80dbcb76162f46abb5358fd0b68426681ee54bba
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-18 19:50:55 +0200

    SplFixedArray extensions

--- app/Poker/Helpers/SplFixedArrayPlus.php
+++ app/Poker/Helpers/SplFixedArrayPlus.php
@@ -7,19 +7,19 @@ use \SplFixedArray;
 
 /**
  * Adding some methods to ease tedium
- * Class SplFixedArrayExtended
+ * Class SplFixedArrayPlus
  * @package App\Poker
  */
-class SplFixedArrayExtended extends SplFixedArray
+class SplFixedArrayPlus extends SplFixedArray
 {
     /**
      * In many cases need to access previous element and then compare to it in some way.
      * Implementation violates scope rules and assumes that given array will not have
      * null values.
      * @param callable $handler
-     * @return SplFixedArrayExtended
+     * @return SplFixedArrayPlus
      */
-    public function applyWithPrevious(callable $handler): SplFixedArrayExtended
+    public function applyWithPrevious(callable $handler): SplFixedArrayPlus
     {
         if (1 < $this->count()) {
             $processedCollection = new self($this->count() - 1);
@@ -36,5 +36,33 @@ class SplFixedArrayExtended extends SplFixedArray
             $processedCollection = new self(0);
         }
         return $processedCollection;
+    }
+
+    public function tail(): SplFixedArrayPlus
+    {
+        return $this->slice(1, $this->count() - 1);
+    }
+
+    /**
+     * Returns a copy of itself except the last element
+     * @return SplFixedArrayPlus
+     */
+    public function tailInverse(): SplFixedArrayPlus
+    {
+        return $this->slice(0, $this->count() - 1);
+    }
+
+    public function slice(int $startIndex, int $length)
+    {
+        $slice = new self(0);
+        if (array_key_exists($startIndex, $this)) {
+            $slice = new self($length);
+            foreach ($this as $key => $value) {
+                if ($startIndex <= $key && $key < $length) {
+                    $slice[$key - $startIndex] = $value;
+                }
+            }
+        }
+        return $slice;
     }
 }
