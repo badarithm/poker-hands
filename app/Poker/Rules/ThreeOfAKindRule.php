0a42d414cd2f9fa2b5b2992b1fc68f8c4a3c84d0
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-19 17:34:29 +0200

    Three of a kind rule implementation

--- app/Poker/Rules/ThreeOfAKindRule.php
+++ app/Poker/Rules/ThreeOfAKindRule.php
@@ -4,19 +4,34 @@
 namespace App\Poker\Rules;
 
 
+use App\Poker\Contracts\CardInterface;
+use App\Poker\Contracts\HandInterface;
 use App\Poker\Contracts\RuleInterface;
-use App\Poker\HandInterface;
+use App\Poker\Helpers\SplFixedArrayExtensionInterface;
 
-class ThreeOfAKindRule implements RuleInterface
+class ThreeOfAKindRule extends AbstractRuleClass
 {
 
+    /**
+     * Three cards are of the same rank and two other cards have different rank.
+     * Suit in this case doesn't really matter
+     * @param HandInterface $hand
+     * @return bool
+     */
     public function applies(HandInterface $hand): bool
     {
-        // TODO: Implement applies() method.
+        $cluster = $hand->getCards()->cluster(function(CardInterface $card) {
+           return array($card->getRank(), true);
+        });
+
+        return 3 === $cluster->count() && $cluster->filter(function(SplFixedArrayExtensionInterface $collection) {
+            $length = $collection->count();
+            return 1 === $length || 3 === $length;
+        });
     }
 
-    public function distance(RuleInterface $other): int
+    public function weight(): int
     {
-        // TODO: Implement distance() method.
+        return 0;
     }
 }
