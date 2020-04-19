1c32c709a795e8f53ead406b4eb73a5d09443e49
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-20 00:28:45 +0200

    Adjustments for upload form

--- resources/views/theme/file-upload-form.blade.php
+++ resources/views/theme/file-upload-form.blade.php
@@ -1,17 +1,25 @@
 @extends('theme.main')
 
 @section('content')
-    <form action="{{route('result-upload-action')}}" method="POST" enctype="multipart/form-data">
-        {{@csrf_field()}}
-        <div class="form-group">
-            <label for="file-upload-button">Please select file</label>
-            <input name="result_file" type="file" class="form-control-file" id="file-upload-button" aria-describedby="file-upload-help-block">
-            <small id="file-upload-help-block" class="form-text text-muted text-danger">
-                @if($errors->has('result_file'))
-                    {{implode(' ', $errors->get('result_file'))}}
-                @endif
-            </small>
+    <div class="col-sm-8 col-md-6 offset-sm-2 offset-md-3">
+        <div class="col-xs-12" style="height:150px;"></div>
+            <div class="card">
+                <div class="card-body">
+                <form action="{{route('result-upload-action')}}" method="POST" enctype="multipart/form-data">
+                    {{@csrf_field()}}
+                    <div class="form-group">
+                        <label for="file-upload-button">Please select file</label>
+                        <input name="result_file" type="file" class="form-control-file" id="file-upload-button" aria-describedby="file-upload-help-block">
+                        <small id="file-upload-help-block" class="form-text text-muted text-danger">
+                            @if($errors->has('result_file'))
+                                {{implode(' ', $errors->get('result_file'))}}
+                            @endif
+                        </small>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Start Upload</button>
+                </form>
+            </div>
         </div>
-        <button type="submit" class="btn btn-primary">Start Upload</button>
-    </form>
+    </div>
+
 @endsection
--- resources/views/theme/main-menu.blade.php
+++ resources/views/theme/main-menu.blade.php
@@ -1,5 +1,5 @@
 @if(auth()->check())
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <a class="navbar-brand" href="">Poker Hands</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
