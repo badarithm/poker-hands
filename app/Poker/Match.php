b2ce2639b847523415008d3eb762db5a82ba48d6
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-19 19:00:02 +0200

    Match class implementation.

--- app/Poker/Match.php
+++ app/Poker/Match.php
@@ -3,6 +3,16 @@
 
 namespace App\Poker\Contracts;
 
+use App\Poker\Rules\FiveOfAKindRule;
+use App\Poker\Rules\FlushRule;
+use App\Poker\Rules\FourOfAKindRule;
+use App\Poker\Rules\FullHouseRule;
+use App\Poker\Rules\HighCardRule;
+use App\Poker\Rules\OnePairRule;
+use App\Poker\Rules\StraightFlushRule;
+use App\Poker\Rules\StraightRule;
+use App\Poker\Rules\ThreeOfAKindRule;
+use App\Poker\Rules\TwoPairRule;
 use \SplFixedArray;
 
 /**
@@ -14,6 +24,25 @@ class Match implements MatchInterface
     const MAX_PLAYERS = 2;
     private $hands = null;
 
+    private $applicableRules = array(
+        FlushRule::class,
+        FiveOfAKindRule::class,
+        FourOfAKindRule::class,
+        FullHouseRule::class,
+        HighCardRule::class,
+        OnePairRule::class,
+        StraightFlushRule::class,
+        StraightRule::class,
+        ThreeOfAKindRule::class,
+        TwoPairRule::class,
+    );
+
+    /**
+     * Stack of rules that have to be applied
+     * @var null
+     */
+    private $ruleQueue = null;
+
     /**
      * Match constructor. Initiates array for hands. Again, using fixed array, because size is known
      * in advance.
@@ -21,6 +50,17 @@ class Match implements MatchInterface
     public function __construct()
     {
         $this->hands = new SplFixedArray(static::MAX_PLAYERS);
+        $this->initiateRules();
+    }
+
+    private function initiateRules(): void
+    {
+        foreach ($this->applicableRules as $classReference) {
+            $instance = new $classReference();
+            $this->ruleQueue[$instance->weight()] = $instance;
+        }
+        // rule with highest weight should be applied earlier
+        krsort($this->ruleQueue);
     }
 
     /**
@@ -28,7 +68,7 @@ class Match implements MatchInterface
      */
     public function addHand(HandInterface $hand): void
     {
-        // TODO: Implement addHand() method.
+        $this->hands[] = $hand;
     }
 
     /**
@@ -36,6 +76,24 @@ class Match implements MatchInterface
      */
     public function pickWinningHand(): HandInterface
     {
-        // TODO: Implement pickWinningHand() method.
+        foreach ($this->ruleQueue as $ruleInstance)
+        {
+            $playerStatus = array();
+            foreach ($this->hands as $key => $hand) {
+                $playerStatus[$key] = $ruleInstance->applies($hand);
+            }
+
+            $numberOfWinners = array_filter($playerStatus, function($status) {
+               return $status;
+            });
+
+            if (1 < $numberOfWinners) {
+                return $ruleInstance->resolve($playerStatus[0], $playerStatus[1]);
+            } elseif ($playerStatus[0]) {
+                return $this->hands[0];
+            } elseif ($this->hands[1]){
+                return $this->hands[1];
+            }
+        }
     }
 }
