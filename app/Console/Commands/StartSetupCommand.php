8374d8135f197c39f445358f6e0ed0cfe7f20bce
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-19 20:32:13 +0200

    Added basic command to automate setup.

--- app/Console/Commands/StartSetupCommand.php
+++ app/Console/Commands/StartSetupCommand.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Hash;
 
 class StartSetupCommand extends Command
 {
@@ -37,7 +41,72 @@ class StartSetupCommand extends Command
      */
     public function handle()
     {
-        $this->confirm('Please confirm that you want to actually apply these changes.');
+        if ($this->confirm('Will create local database inside project, apply migrations and create user account. Then it will ask for port number and start local development server. Please confirm.')) {
+            $this->info('Creating application key...');
+//            Artisan::call('config:create-env-file');
+            Artisan::call('key:generate');
+            $this->info('Will create SQLite database...');
+//            Artisan::call('database:create-local');
+            $this->info('Created SQLite database.');
+            $this->info('Will migrate database tables...');
+            Artisan::call('migrate:fresh');
+            $this->info('Migrations were completed.');
+            $this->info('Will create default user account.');
 
+            $name = $this->getInput('Please enter your name. One letter is considered sufficient.');
+            $surname = $this->getInput('Please enter your surname. Again, letter is considered sufficient.');
+            $email = $this->getEmail();
+            $password = $this->getPassword();
+            User::create([
+                'name' => $name,
+                'surname' => $surname,
+                'email' => $email,
+                'password' => Hash::make($password),
+            ]);
+            $this->info("User {$email} was created.");
+            $this->info('Setup is ready.');
+            $this->info('Will start local development server now.');
+            $port = $this->getPort();
+            Artisan::call("serve --port={$port}");
+
+        } else {
+            $this->info('You chose no to proceed. No changes were applied.');
+        }
+    }
+
+    protected function getEmail(): string
+    {
+        $email = $this->ask('Please enter email for admin user.');
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            return $this->getEmail();
+        }
+        return $email;
+    }
+
+    public function getPassword(): string
+    {
+        $password = $this->secret('Please enter password. Should consist of at least 6 letters and / or numbers.');
+        if (!ctype_alnum($password) && 6 >= strlen(trim($password))) {
+            return $this->getPassword();
+        }
+        return $password;
+    }
+
+    public function getInput(string $message): string
+    {
+        $input = $this->ask($message);
+        if (!ctype_alpha($input) && 1 >= strlen(trim($input))) {
+            return $this->getInput($message);
+        }
+        return $input;
+    }
+
+    public function getPort(): string
+    {
+        $input = $this->ask('Please provide port number that is available on your local machine.');
+        if (!ctype_digit($input) && 1 >= strlen(trim($input))) {
+            return $this->getPort();
+        }
+        return $input;
     }
 }
