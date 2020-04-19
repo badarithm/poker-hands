3108effc565810618295e836bef1cbfb1ce2c6fb
Karolis Markevicius <karolis.markevicius@protonmail.ch>
2020-04-20 00:36:10 +0200

    Show victories on the left side

--- resources/views/theme/main-menu.blade.php
+++ resources/views/theme/main-menu.blade.php
@@ -7,9 +7,18 @@
         <div class="collapse navbar-collapse" id="main-navbar">
             <div class="navbar-nav">
                 <a class="nav-item nav-link" href="{{route('dashboard')}}">Dashboard</a>
-                <a class="nav-item nav-link" href="{{route('result-upload-form')}}">Upload Results</a>
+                <a class="nav-item nav-link" href="{{route('result-upload-form')}}">Upload</a>
                 <a class="nav-item nav-link" href="{{route('logout')}}">Logout</a>
             </div>
+        </div>
+        <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <div class="btn btn-light">
+                        Victories <span class="badge badge-danger">4</span>
+                    </div>
+                </li>
+            </ul>
         </div>
     </nav>
 @endif
